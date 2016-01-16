1348c5156772c55e94ead1d43f3501fb760b5465
Krzysiek <poccczta@wp.pl>
2016-01-16 17:40:18 +0100

    poprawa błędu

--- GITHUB1/src/Runner1.java
+++ GITHUB1/src/Runner1.java
@@ -4,7 +4,7 @@ public class Runner1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GITHUB");
-		System.out.println("DUPADUPA2");
+		System.out.println("DUPADUPA2"); 
 	}
 
 }
